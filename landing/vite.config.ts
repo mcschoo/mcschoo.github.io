f117cd052ad728df155d92de7450ded2fe2290c2
mcschoo <mcschoo@clemson.edu>
2026-01-05 18:18:17 -0500

    debug vite errors

--- landing/vite.config.ts
+++ landing/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
     // Set base path for GitHub Pages - replace 'REPO_NAME' with your actual repository name
-  base: mode === "production" ? "/mcschoo.github.io/landing/" : "/",
+  base: mode === "production" ? "/landing/" : "/",
   server: {
     host: "::",
     port: 8080,
